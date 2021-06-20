ccb21ebbef46e10808d96b985fd5138cd8c4ec25
aseem09 <averma2@cs.iitr.ac.in>
2021-06-21 03:15:56 +0530

    Add js code

---
+++ js/script.js
@@ -0,0 +1,176 @@
+var url = "https://gist.github.com/aseem09/8f6667526ad0deab0bd12b3c38e1fe9f/raw";
+
+async function getData() {
+    const response = await fetch(url, {
+        headers: {
+            // 'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    });
+    var data = await response.json();
+    const res = toGistResponse(data)["projects"];
+    console.log(res);
+    document.getElementById("head-1").innerHTML = res[0]["name"];
+    document.getElementById("head-2").innerHTML = res[1]["name"];
+    document.getElementById("head-3").innerHTML = res[2]["name"];
+    
+    document.getElementById("topic-1").innerHTML = res[0]["topic"];
+    document.getElementById("topic-2").innerHTML = res[1]["topic"];
+    document.getElementById("topic-3").innerHTML = res[2]["topic"];
+    
+}
+
+function toGistResponse(json) {
+    return cast(json, r("GistResponse"));
+}
+
+function gistResponseToJson(value) {
+    return JSON.stringify(uncast(value, r("GistResponse")), null, 2);
+}
+
+function invalidValue(typ, val, key = '') {
+    if (key) {
+        throw Error(`Invalid value for key "${key}". Expected type ${JSON.stringify(typ)} but got ${JSON.stringify(val)}`);
+    }
+    throw Error(`Invalid value ${JSON.stringify(val)} for type ${JSON.stringify(typ)}`, );
+}
+
+function jsonToJSProps(typ) {
+    if (typ.jsonToJS === undefined) {
+        const map = {};
+        typ.props.forEach((p) => map[p.json] = { key: p.js, typ: p.typ });
+        typ.jsonToJS = map;
+    }
+    return typ.jsonToJS;
+}
+
+function jsToJSONProps(typ) {
+    if (typ.jsToJSON === undefined) {
+        const map = {};
+        typ.props.forEach((p) => map[p.js] = { key: p.json, typ: p.typ });
+        typ.jsToJSON = map;
+    }
+    return typ.jsToJSON;
+}
+
+function transform(val, typ, getProps, key = '') {
+    function transformPrimitive(typ, val) {
+        if (typeof typ === typeof val) return val;
+        return invalidValue(typ, val, key);
+    }
+
+    function transformUnion(typs, val) {
+        // val must validate against one typ in typs
+        const l = typs.length;
+        for (let i = 0; i < l; i++) {
+            const typ = typs[i];
+            try {
+                return transform(val, typ, getProps);
+            } catch (_) {}
+        }
+        return invalidValue(typs, val);
+    }
+
+    function transformEnum(cases, val) {
+        if (cases.indexOf(val) !== -1) return val;
+        return invalidValue(cases, val);
+    }
+
+    function transformArray(typ, val) {
+        // val must be an array with no invalid elements
+        if (!Array.isArray(val)) return invalidValue("array", val);
+        return val.map(el => transform(el, typ, getProps));
+    }
+
+    function transformDate(val) {
+        if (val === null) {
+            return null;
+        }
+        const d = new Date(val);
+        if (isNaN(d.valueOf())) {
+            return invalidValue("Date", val);
+        }
+        return d;
+    }
+
+    function transformObject(props, additional, val) {
+        if (val === null || typeof val !== "object" || Array.isArray(val)) {
+            return invalidValue("object", val);
+        }
+        const result = {};
+        Object.getOwnPropertyNames(props).forEach(key => {
+            const prop = props[key];
+            const v = Object.prototype.hasOwnProperty.call(val, key) ? val[key] : undefined;
+            result[prop.key] = transform(v, prop.typ, getProps, prop.key);
+        });
+        Object.getOwnPropertyNames(val).forEach(key => {
+            if (!Object.prototype.hasOwnProperty.call(props, key)) {
+                result[key] = transform(val[key], additional, getProps, key);
+            }
+        });
+        return result;
+    }
+
+    if (typ === "any") return val;
+    if (typ === null) {
+        if (val === null) return val;
+        return invalidValue(typ, val);
+    }
+    if (typ === false) return invalidValue(typ, val);
+    while (typeof typ === "object" && typ.ref !== undefined) {
+        typ = typeMap[typ.ref];
+    }
+    if (Array.isArray(typ)) return transformEnum(typ, val);
+    if (typeof typ === "object") {
+        return typ.hasOwnProperty("unionMembers") ? transformUnion(typ.unionMembers, val)
+            : typ.hasOwnProperty("arrayItems")    ? transformArray(typ.arrayItems, val)
+            : typ.hasOwnProperty("props")         ? transformObject(getProps(typ), typ.additional, val)
+            : invalidValue(typ, val);
+    }
+    // Numbers can be parsed by Date but shouldn't be.
+    if (typ === Date && typeof val !== "number") return transformDate(val);
+    return transformPrimitive(typ, val);
+}
+
+function cast(val, typ) {
+    return transform(val, typ, jsonToJSProps);
+}
+
+function uncast(val, typ) {
+    return transform(val, typ, jsToJSONProps);
+}
+
+function a(typ) {
+    return { arrayItems: typ };
+}
+
+function u(...typs) {
+    return { unionMembers: typs };
+}
+
+function o(props, additional) {
+    return { props, additional };
+}
+
+function m(additional) {
+    return { props: [], additional };
+}
+
+function r(name) {
+    return { ref: name };
+}
+
+const typeMap = {
+    "GistResponse": o([
+        { json: "projects", js: "projects", typ: a(r("Project")) },
+    ], false),
+    "Project": o([
+        { json: "name", js: "name", typ: "" },
+        { json: "topic", js: "topic", typ: "" },
+    ], false),
+};
+
+// module.exports = {
+//     "gistResponseToJson": gistResponseToJson,
+//     "toGistResponse": toGistResponse,
+// };
